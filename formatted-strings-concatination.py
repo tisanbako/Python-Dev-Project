04ddff57196753a6c3fa3f5d98a0b19e5c99eed2
tisanbako <bakotisan@gmail.com>
2024-05-30 17:31:11 -0400

    adding files

---
+++ formatted-strings-concatination.py
@@ -0,0 +1,10 @@
+first_name = "John"
+last_name = "Bako"
+#Lets say we want to print john [Bako] is a coder 
+#with concatination would be 
+message = first_name + " [" + last_name + "] is a coder"
+print(message)
+
+#with formatted string  would be 
+msg = f"{first_name} [{last_name}] is a coder"
+print(msg)
---
+++ quotation.py
@@ -0,0 +1,24 @@
+course1 = "this is python's course for beginners"
+print (course1)
+
+course2 = 'python course for "beginners"'
+print(course2)
+
+#to define a string with multiple line, we use threeple quotes eg
+course3 = ''' #we can use sinlge or double quotation
+Hi John,
+Here is our first email to you.
+
+Thank you,
+The support team
+''' 
+print(course3)
+
+course4 = "python for beginers"
+print(course4[4])
+print(course4[-2])
+print(course4[0:3])
+print(course4[2:])
+print(course4[:5])
+print(course4[1:-1])
+
