2ad0d6346c91f3a8981a02a07685087cb214895b
tisanbako <bakotisan@gmail.com>
2024-05-31 02:21:43 -0400

    adding files

---
+++ 8. math_function.py
@@ -0,0 +1,18 @@
+
+#in python we have lots of built in functions for performing mathematical operations
+x = 2.9
+round(x)
+print(x)
+
+m = 3.9
+print(round(m))
+
+
+#whenever you want to write a program that involves complex mathematical calculation, you will 
+#need to import the math module. A module in python is a seperate file with some re-useable code.
+#like...
+import math
+print(math.ceil(2.8))
+print(math.floor(4.7))
+
+#The math module has lots of functions built in it, google python3 math function to see them.
