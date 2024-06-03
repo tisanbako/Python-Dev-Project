46bdf014ca2da95e713b3ee609556d321fb91161
tisanbako <bakotisan@gmail.com>
2024-06-02 22:46:45 -0400

    adding files

--- 12. comparison_operators.py
+++ 12. comparison_operators.py
@@ -22,3 +22,24 @@ elif temperature < 10:
 else:
     print("it's neigher hot not cold") 
    
+print("Have a good day")
+   
+#EX2. 
+#If name is less than 3 characters long
+      #name must be at least 3 characters
+#otherwise if it's more than 50 characters long
+      #name can be a maximum of 50 characters
+#otherwise
+     #name looks good!
+
+
+name = input("What is your name? ")
+name_characters = len(name)
+
+if name_characters < 3:
+    print("Name must be atleast 3 characters")
+elif name_characters > 50:
+    print("Name can only be a maximum of 50 characters")
+else:
+    print("Name looks good!")
+
